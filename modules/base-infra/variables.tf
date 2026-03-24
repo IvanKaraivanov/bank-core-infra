fcedf0ea918ff44fdde5c0bdbf35844fd0552abd
Ivan Karaivanov <ivan.a.karaivanov@gmail.com>
2026-03-24 09:25:28 +0100

    feat: add databricks subnets and data lake storage

--- modules/base-infra/variables.tf
+++ modules/base-infra/variables.tf
@@ -8,3 +8,15 @@ variable "location" {
   type        = string
   default     = "westeurope"
 }
+
+variable "dbx_public_subnet_prefix" {
+  type        = string
+  description = "Address prefix for Databricks Public (Host) Subnet"
+  default     = "10.0.1.0/24"
+}
+
+variable "dbx_private_subnet_prefix" {
+  type        = string
+  description = "Address prefix for Databricks Private (Container) Subnet"
+  default     = "10.0.2.0/24"
+}
