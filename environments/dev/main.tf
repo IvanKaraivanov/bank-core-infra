83ef1ea13dcf97f39a09841f655bd09c14b29c96
Ivan Karaivanov <ivan.a.karaivanov@gmail.com>
2026-03-27 09:42:02 +0100

    Add admin group with full permissions on storage credential and external location

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -2,4 +2,9 @@ module "base_infra" {
   source      = "../../modules/base-infra"
   environment = "dev"
   location    = "westeurope"
+  
+  # Add your email here to be included in the admin group
+  databricks_admin_users = [
+    "nextgenlearning@ivanakaraivanovgmail.onmicrosoft.com"
+  ]
 }
